733481c34a104cc13c1c0fe9776ae180911e2d5d
Danny-sth <danny.wasth@gmail.com>
2021-09-08 09:35:57 +0300

    Сконфигурировал проект

---
+++ src/main/java/com/dannykudinov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.dannykudinov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
