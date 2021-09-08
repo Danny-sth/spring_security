980aa33be198a13eaee23c8555ecd88863efee31
Danny-sth <danny.wasth@gmail.com>
2021-09-08 10:52:53 +0300

    Поключил БД и перенес в нее username и password

--- src/main/java/com/dannykudinov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/dannykudinov/spring/security/configuration/MySecurityConfig.java
@@ -1,25 +1,22 @@
 package com.dannykudinov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("denis")
-                        .password("denis").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena")
-                        .password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan")
-                        .password("ivan").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
